e0803f0ea4b20c681c8b4c2310a7e464936d2da8
PC-LIFE <PC-LIFE@WESLEY>
2021-12-06 11:04:15 -0300

    decorator xd

--- PBomberManUSFX/Scenes/LevelScene.h
+++ PBomberManUSFX/Scenes/LevelScene.h
@@ -20,6 +20,16 @@
 #include "../Factories/FactoryGameClasico.h"
 #include "../Factories/FactoryGameCartoon.h"
 #include "../Entities/ClasicoPlayer.h"
+#include "../GameVersions.h"
+#include "../GameTextures.h"
+
+#include "../Adapters/WallPacman.h"
+#include "../Adapters/TileGraph.h"
+#include "../Adapters/Tile.h"
+
+#include "../Decorator/DecoratorWall.h"
+#include "../Decorator/BorderDecoratorWall.h"
+#include "../Decorator/ShineDecoratorWall.h"
 
 /**
     * @brief Level Scene
@@ -36,7 +46,9 @@ public:
         * @param game - game pointer
         * @param stage - stage number
         */
-    LevelScene(GameManager* game, const unsigned int stage, const unsigned int prevScore);
+    LevelScene(GameManager* game, const unsigned int _stage, const unsigned int _prevScore);
+    LevelScene(GameManager* game, GameVersion _gameVersion, const unsigned int _stage, const unsigned int _prevScore);
+    
     /**
         * @brief Catch SDL2 events
         *
@@ -61,8 +73,10 @@ private:
     void spawnGrass(const int positionX, const int positionY);
     void spawnBrick(const int positionX, const int positionY);
     void spawnStone(const int positionX, const int positionY);
+    void spawnWallPacman(const int positionX, const int positionY, Tile* _tile);
+
     void spawnPlayer(const int positionX, const int positionY);
-    void spawnEnemy(Texture texture, AIType type, const int positionX, const int positionY);
+    void spawnEnemy(GameTexture texture, AIType type, const int positionX, const int positionY);
     void spawnBomb(GameGraphicObject* object);
     void spawnBang(GameGraphicObject* object);
     void spawnDoor(GameGraphicObject* object);
@@ -128,9 +142,9 @@ private:
     std::shared_ptr<Sprite> bomb = nullptr;                           // player's bomb
     std::shared_ptr<Sprite> door = nullptr;                           // door for level finish
     std::vector<std::shared_ptr<Enemy>> enemies;                      // enemies
-    std::vector<std::pair<Tile, std::shared_ptr<GameGraphicObject>>> collisions; // collisions
+    std::vector<std::pair<GameTile, std::shared_ptr<GameGraphicObject>>> collisions; // collisions
     std::vector<std::shared_ptr<GameGraphicObject>> bangs;                       // bomb's bang
-    Tile tiles[tileArrayHeight][tileArrayWidth];                      // tilemap
+    GameTile tiles[tileArrayHeight][tileArrayWidth];                      // tilemap
 
 
     int playerDirectionX = 0; // direction used for control
@@ -146,6 +160,7 @@ private:
     bool isPaused = false;
     bool isWin = false;
     // variables
+    unsigned int gameVersion = GAMEVERSION_CLASIC;
     unsigned int score = 0;
     unsigned int stage = 0;
 
@@ -156,6 +171,8 @@ private:
     int scaledTileSize = 0;
     // last object that used as background (grass)
     int backgroundObjectLastNumber = 0;
+
+    TileGraph* tileGraph;
 };
 
 #endif // _BOMBERMAN_LEVEL_SCENE_H_
